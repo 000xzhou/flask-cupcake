3bd19a165e1226546f8a17f383393ac275439cc0
000xzhou <xzhou92@gmail.com>
2024-01-25 20:44:46 -0500

    added delete

--- static/app.js
+++ static/app.js
@@ -1,4 +1,6 @@
 // todo: Refactor your front-end code to be object-oriented using class methods to fetchAllCupcakes and createCupcakes and instance methods for updating and deleting cupcakes as well as searching for cupcakes.
+
+// POST
 const addCupcakeForm = document.getElementById("add-cupcake");
 
 addCupcakeForm.addEventListener("submit", addcupcake);
@@ -34,6 +36,29 @@ async function addcupcake(e) {
     }
   } catch (error) {
     // Handle error
+    console.error(
+      "Error Code:",
+      error.code,
+      "\nError Name:",
+      error.name,
+      "\nError Message:",
+      error.message
+    );
+  }
+}
+
+// DELETE
+deleteBtns = document.querySelectorAll(".delete-btn");
+
+deleteBtns.forEach((btn) => {
+  btn.addEventListener("click", deleteCupcake);
+});
+async function deleteCupcake() {
+  try {
+    let id = this.parentElement.dataset.id;
+    const response = await axios.delete(`/api/cupcakes/${id}`);
+    console.log("Response:", response.data);
+  } catch (error) {
     console.error(
       "Error Code:",
       error.code,
