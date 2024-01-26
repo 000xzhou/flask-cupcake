469fe4e8aab10e182cc74b174da5dd9c5e9bcabe
000xzhou <xzhou92@gmail.com>
2024-01-25 19:13:32 -0500

    fix form

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, Optional
 
 class AddCupcake(FlaskForm):
     """Form for adding cupcakes."""
-    flavor = StringField("Name", validators=[InputRequired(message="Must include a flavor")])
-    size = [("small", "small"), ("medium", "medium"), ("large", "large")]
-    rating = FloatField("Age", validators=[Optional(), validators.NumberRange(min=0, max=10, message="rating must be between 0 and 10")])
-    image = StringField("Photo URL", validators=[Optional(), validators.URL(message="Must be a URL")])
+    flavor = StringField("Flavor", validators=[InputRequired(message="Must include a flavor")])
+    size = SelectField("Size", choices=[("small", "small"), ("medium", "medium"), ("large", "large")])  
+    rating = FloatField("Rating", validators=[Optional(), validators.NumberRange(min=0, max=10, message="rating must be between 0 and 10")])
+    image = StringField("Image", validators=[Optional(), validators.URL(message="Must be a URL")])
